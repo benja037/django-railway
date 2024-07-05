8ba26f19c90e3ee9ddcb5c2e759e3b1bec1ed281
benjamin <benja037@gmail.com>
2024-07-05 00:01:26 -0400

    Add static root to settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -174,6 +174,7 @@ DJOSER = {
     "LOGIN_FIELD": "email",
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
